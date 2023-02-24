0d1ac1d0b8dd1d277f186a65cf0ea540b3afec5e
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-02-24 16:03:35 +0300

    static functions cleared from the router

--- src/routes/products.js
+++ src/routes/products.js
@@ -5,13 +5,11 @@ import {
   addProduct,
   updateProduct,
   deleteProduct,
-  getProductsStatic,
 } from '../controllers/products-controller.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(addProduct);
-router.route('/static').get(getProductsStatic);
 router
   .route('/:id')
   .get(getProductById)
