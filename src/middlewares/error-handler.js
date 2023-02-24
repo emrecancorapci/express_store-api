d750feb5f363fd0d089e7fef17fc62abef55db86
Emre Can Çorapçı <emrecancorapci@gmail.com>
2023-02-24 16:03:09 +0300

    package update, error handlers implemented

--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -2,13 +2,8 @@ import { APIError } from '../errors/api-error.js';
 
 export default (err, req, res) => {
   if (err instanceof APIError) {
-    return res.status(err.statusCode).json({
-      msg: err.message,
-    });
+    return res.status(err.statusCode).json({ msg: err.message });
   }
-
-  return res.status(err.status).json({
-    error: 'Server error',
-    msg: err.message,
-  });
+  return res.status(500).send('Something went wrong try again later');
 };
+
